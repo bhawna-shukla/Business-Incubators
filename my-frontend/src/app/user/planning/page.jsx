62c41a545ea7e02526cca2977fbdf7142658b8da
Bhawna Shukla <shuklabhawna81@gmail.com>
2025-05-15 10:00:25 +0530

    service launching

--- my-frontend/src/app/user/planning/page.jsx
+++ my-frontend/src/app/user/planning/page.jsx
@@ -71,7 +71,66 @@ const Planning = () => {
           </div>
         </section>
 
-
+   { <section className="bg-white ">
+  <div className="container px-6 py-10 mx-auto">
+    <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-gray-900">
+      More websites 
+    </h1>
+    <p className="mt-4 text-center text-gray-500 dark:text-gray-900">
+      
+    </p>
+    <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+      <div
+        className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+        style={{
+          backgroundImage:
+            'url("https://media.designrush.com/articles/207772/conversions/Best-Single-Page-details.jpg")'
+        }}
+      >
+        <div className="flex flex-col justify-cjenter w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+          <h2 className="mt-4 text-xl font-semibold text-white capitalize">
+            Best website collections
+          </h2>
+          <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase ">
+            Website
+          </p>
+        </div>
+      </div>
+      <div
+        className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+        style={{
+          backgroundImage:
+            'url("https://cdn.psdrepo.com/images/2x/fashion-influencer-ui-kit-for-adobe-xd-g1.jpg")'
+        }}
+      >
+        <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+          <h2 className="mt-4 text-xl font-semibold text-white capitalize">
+            Block of Ui kit collections
+          </h2>
+          <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase ">
+            Ui kit
+          </p>
+        </div>
+      </div>
+      <div
+        className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+        style={{
+          backgroundImage:
+            'url("https://img.freepik.com/premium-psd/phone-screen-mockup-psd-with-hand-holding-aesthetic-beige-widgets_53876-111326.jpg")'
+        }}
+      >
+        <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+          <h2 className="mt-4 text-xl font-semibold text-white capitalize">
+            Ton’s of mobile mockup
+          </h2>
+          <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase ">
+            Mockups
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section> }
 
 
 
